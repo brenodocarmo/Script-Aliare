325622d64fbf416e25b4f260f78380a2d2323e3d
Breno do Carmo Sousa <48214582+brenodocarmo@users.noreply.github.com>
2025-03-19 10:20:23 -0300

    Update acompanhamento_ordem_producao.sql
    Alteração na descrição das colunas

--- Agroindustria/Ordem de Producao/acompanhamento_ordem_producao.sql
+++ Agroindustria/Ordem de Producao/acompanhamento_ordem_producao.sql
@@ -8,52 +8,54 @@ Data: 22/10/2024
 
 */
 
-SELECT 
-    acompanhaordem.codi_ord ordem_producao,
+CREATE OR REPLACE VIEW VW_ORDEM_PRODUCAO AS 
 
-    -- Definição da situação da Ordem de Produção
-    CASE
-        WHEN ordemproducao.stat_ord = 'A' THEN 'Aguardando'
-        WHEN ordemproducao.stat_ord = 'P' THEN 'Paralizada'
-        WHEN ordemproducao.stat_ord = 'E' THEN 'Iniciada'
-        WHEN ordemproducao.stat_ord = 'F' THEN 'Finalizada'
-        WHEN ordemproducao.stat_ord = 'C' THEN 'Cancelada'
-    END situacao_op,
-    acompanhaordem.codi_eta codigo_etapa,
-    etapa.desc_eta descricao_etapa,
+    SELECT 
+        acompanhaordem.codi_ord ord_prod,
     
-    acompanhaordem.dini_aco data_inicio,    
-    -- Extração da Hora, Minutos e Segunda de cada inicio de etapa.
-    EXTRACT(HOUR FROM (acompanhaordem.hini_aco))||':' ||EXTRACT(minute FROM (acompanhaordem.hini_aco))||':'||EXTRACT(second FROM (acompanhaordem.hini_aco)) hora_inicio,
-
- 
-    -- Definição da situação da Etapa de Produção
-    CASE
-        WHEN acompanhaordem.stat_aco = 'I' THEN 'Iniciada'
-        WHEN acompanhaordem.stat_aco = 'C' THEN 'Criada'
-        WHEN acompanhaordem.stat_aco = 'F' THEN 'Finalizada'
-    END situacao_etapa,
+        -- Definição da situação da Ordem de Produção
+        CASE
+            WHEN ordemproducao.stat_ord = 'A' THEN 'Aguardando'
+            WHEN ordemproducao.stat_ord = 'P' THEN 'Paralizada'
+            WHEN ordemproducao.stat_ord = 'E' THEN 'Iniciada'
+            WHEN ordemproducao.stat_ord = 'F' THEN 'Finalizada'
+            WHEN ordemproducao.stat_ord = 'C' THEN 'Cancelada'
+        END situ_op,
+        acompanhaordem.codi_eta cod_eta,
+        etapa.desc_eta desc_etapa,
+        
+        acompanhaordem.dini_aco data_ini,    
+        -- Extração da Hora, Minutos e Segunda de cada inicio de etapa.
+        EXTRACT(HOUR FROM (acompanhaordem.hini_aco))||':' ||EXTRACT(minute FROM (acompanhaordem.hini_aco))||':'||EXTRACT(second FROM (acompanhaordem.hini_aco)) hora_ini,
     
-    acompanhaordem.dfim_aco data_termino,
-    -- Extração da Hora, Minutos e Segunda de cada fim de etapa.
-    EXTRACT(HOUR FROM (acompanhaordem.hfim_aco))||':' ||EXTRACT(minute FROM (acompanhaordem.hfim_aco))||':'||EXTRACT(second FROM (acompanhaordem.hfim_aco)) hora_termino,
+     
+        -- Definição da situação da Etapa de Produção
+        CASE
+            WHEN acompanhaordem.stat_aco = 'I' THEN 'Iniciada'
+            WHEN acompanhaordem.stat_aco = 'C' THEN 'Criada'
+            WHEN acompanhaordem.stat_aco = 'F' THEN 'Finalizada'
+        END situ_eta,
+        
+        acompanhaordem.dfim_aco data_fim,
+        -- Extração da Hora, Minutos e Segunda de cada fim de etapa.
+        EXTRACT(HOUR FROM (acompanhaordem.hfim_aco))||':' ||EXTRACT(minute FROM (acompanhaordem.hfim_aco))||':'||EXTRACT(second FROM (acompanhaordem.hfim_aco)) hora_fim,
+        
+        iordem.codi_aco cod_acom,
+        iordem.codi_psv cod,
+        prodserv.desc_psv prod,
+        prodserv.unid_psv uni,
+        iordem.qsol_pro qtd_sol,
+        iordem.quti_pro qtd_uti,
+        -- Diferença entre a quantidade solicitada e a quantidade utilizada na Ordem de Produção
+        round((iordem.qsol_pro - iordem.quti_pro),2) dif,
+        iordem.codi_dpt dep,
+        iordem.pcun_pro preco_unit_acabado
     
-    iordem.codi_aco codigo_acompanhamento,
-    iordem.codi_psv codigo,
-    prodserv.desc_psv produto,
-    prodserv.unid_psv unidade,
-    iordem.qsol_pro qtd_solicitada,
-    iordem.quti_pro qtd_utilizada,
-    -- Diferença entre a quantidade solicitada e a quantidade utilizada na Ordem de Produção
-    round((iordem.qsol_pro - iordem.quti_pro),2) diferenca,
-    iordem.codi_dpt deposito,
-    iordem.pcun_pro preco_unit_acabado
-
-FROM iordem
-
-INNER JOIN prodserv ON (iordem.codi_psv = prodserv.codi_psv)
-INNER JOIN acompanhaordem ON (iordem.codi_aco = acompanhaordem.codi_aco)
-INNER JOIN etapa ON (acompanhaordem.codi_eta = etapa.codi_eta)
-INNER JOIN ordemproducao ON (acompanhaordem.codi_ord = ordemproducao.codi_ord)
-
+    FROM iordem
+    
+    INNER JOIN prodserv ON (iordem.codi_psv = prodserv.codi_psv)
+    INNER JOIN acompanhaordem ON (iordem.codi_aco = acompanhaordem.codi_aco)
+    INNER JOIN etapa ON (acompanhaordem.codi_eta = etapa.codi_eta)
+    INNER JOIN ordemproducao ON (acompanhaordem.codi_ord = ordemproducao.codi_ord)
+    
 ORDER BY acompanhaordem.codi_ord, acompanhaordem.codi_eta;
